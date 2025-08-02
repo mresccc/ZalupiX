8fac06e582f8369e8caa322000e9efb678649a59
burlak1n <roma.s-2012@yandex.ru>
2025-08-02 11:01:40 +0300

    Добавлено использование ORJSON для ускорения работы с JSON, улучшено форматирование кода и добавлен TODO для проверки доступности Google API.

--- api.py
+++ api.py
@@ -1,19 +1,27 @@
-from fastapi import FastAPI, Request
-from service.google_data import init_scheduler
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from config import GRID_CREDENTIALS_PATH, SPREADSHEET_URL
+from fastapi.responses import ORJSONResponse
 
+from config import GRID_CREDENTIALS_PATH, SPREADSHEET_URL
+from service.google_data import init_scheduler
 from service.models import Event
+
 scheduler = init_scheduler(SPREADSHEET_URL, GRID_CREDENTIALS_PATH)
 
 app = FastAPI(
-    title="Google Sheets Events API"
+    title="Google Sheets Events API",
+    # ORJSON для более быстрой работы с json
+    default_response_class=ORJSONResponse
 )
+
 
 # Разрешить CORS если надо
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", 'http://localhost:3000'],  # или список разрешённых доменов
+    allow_origins=[
+        "http://localhost:8000",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,7 +30,8 @@ app.add_middleware(
 
 @app.get("/health")
 async def index():
-    return 'health'
+    # TODO: проверять доступность google api
+    return "health"
 
 
 @app.get("/schedule", response_model=list[Event])
