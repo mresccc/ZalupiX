463905cea789c9845593a30a43980ac43ac745f2
burlak1n <roma.s-2012@yandex.ru>
2025-08-02 10:56:16 +0300

    Изменен маршрут корневого API на /health для проверки состояния сервиса, улучшен форматирование кода и исправлены аннотации типов в ответах.

--- api.py
+++ api.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI, Request
 from service.google_data import init_scheduler
 from fastapi.middleware.cors import CORSMiddleware
 from config import GRID_CREDENTIALS_PATH, SPREADSHEET_URL
-from aiogram.types import Update
 
-from bot import bot, dp
-from typing import List
 from service.models import Event
 scheduler = init_scheduler(SPREADSHEET_URL, GRID_CREDENTIALS_PATH)
 
-app = FastAPI(title="Google Sheets Events API")
+app = FastAPI(
+    title="Google Sheets Events API"
+)
 
 # Разрешить CORS если надо
 app.add_middleware(
@@ -21,15 +20,11 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.get("/health")
+async def index():
+    return 'health'
+
+
+@app.get("/schedule", response_model=list[Event])
 async def read_events():
-    return 'Hello'
-
-
-@app.get("/schedule", response_model=List[Event])
-async def read_events():
-
-    events = scheduler.get_events_from_google_sheet()
-    
-    return events
-
+    return scheduler.get_events_from_google_sheet()
